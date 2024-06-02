4c8116d3034cc12caee9c3cf4a6c277d2df7bc51
Priya Chhabra <pcamz1996@gmail.com>
2024-06-02 21:14:01 +0530

    Update SQL1.sql

--- SQL1.sql
+++ SQL1.sql
@@ -1,3 +1,31 @@
 -- SQL Basics
 /* 
   */
+
+--SQL Hardcore Practice 
+
+--Create new Database 
+
+CREATE DATABASE SQL_START
+GO
+
+--Use Database DSA
+USE SQL_START
+
+--Drop database
+DROP DATABASE DSA
+
+--DUMMY TABLE 
+CREATE TABLE Employee(
+EMP_ID INT IDENTITY(1241,3)PRIMARY KEY,
+First_NAME VARCHAR(50) NOT NULL,
+LastName VARCHAR(80) NOT NULL,
+POSTCODE INT,
+PHONE_NUM INT DEFAULT '999999999'
+)
+
+--Selecting from employee 
+SELECT * 
+FROM Employee
+
+

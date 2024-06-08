a13aa024d3629acff3c2478f82382e1e5ef92929
Priya Chhabra <pcamz1996@gmail.com>
2024-06-08 14:07:29 +0530

    Update SQL1.sql

--- SQL1.sql
+++ SQL1.sql
@@ -1,7 +1,3 @@
--- SQL Basics
-/* 
-  */
-
 --SQL Hardcore Practice 
 
 --Create new Database 
@@ -12,7 +8,7 @@ GO
 --Use Database DSA
 USE SQL_START
 
---Drop database
+--Drop database - Drops data and structure 
 DROP DATABASE DSA
 
 --DUMMY TABLE 
@@ -28,9 +24,37 @@ PHONE_NUM INT DEFAULT '999999999'
 SELECT * 
 FROM Employee
 
---Alter Table
-ALTER TABLE Employee 
-ADD GENDER CHAR(1)
+--Alter Table Employee
+ALTER TABLE EMPLOYEE
+ADD Gender char(1)
 
+ALTER TABLE EMPLOYEE
+ALTER COLUMN GENDER VARCHAR(6)
 
+ALTER TABLE EMPLOYEE 
+DROP COLUMN Gender
+
+--Inserting data 
+--Method 1
+
+INSERT INTO EMPLOYEE VALUES
+('Priya','Chhabra',110059,1234567890)
+
+--Method 2
+INSERT INTO Employee(LastName,First_NAME,PHONE_NUM,POSTCODE) VALUES
+('Smith','Rebecca',987456321,110089)
+
+--Updating Values 
+
+--Method 1 Where clause
+UPDATE Employee
+SET 
+PHONE_NUM = 829371646
+WHERE
+EMP_ID = 1241
+
+--Method 2 --As there is no clause all rows got affected with changes
+UPDATE Employee
+SET
+PHONE_NUM = 111111111
 
